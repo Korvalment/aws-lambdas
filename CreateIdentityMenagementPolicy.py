874c393bfc0e65fab2d977c1b461960461993a08
cxnajder <98214055+cxnajder@users.noreply.github.com>
2023-04-05 22:24:22 +0200

    Update CreateIdentityMenagementPolicy.py

--- CreateIdentityMenagementPolicy.py
+++ CreateIdentityMenagementPolicy.py
@@ -31,6 +31,7 @@ def lambda_handler(event, context):
     )
 
 
-    response = json.loads(json.dumps(response, default=str))
+    #response = json.loads(json.dumps(response, default=str))
+    return response["Policy"]["Arn"]
     
     return response
