d9a6cb9a15143c9b8bab3e8646483adfb54cee81
cxnajder <98214055+cxnajder@users.noreply.github.com>
2023-04-05 20:47:10 +0200

    copied form amplify-app/lambda

---
+++ CreateUserPool.py
@@ -0,0 +1,30 @@
+import boto3
+import json
+# from datetime import datetime
+
+def lambda_handler(event, context):
+   # Set the Cognito User Pool settings
+    user_pool_name = event['key1']
+    policies = {
+   "PasswordPolicy": {
+            "MinimumLength": 6,
+            "RequireLowercase": False,
+            "RequireNumbers": False,
+            "RequireSymbols": False,
+            "RequireUppercase": False
+        }
+    }
+    username_attributes = ["email"]
+    
+    # Create a Cognito User Pool
+    cognito = boto3.client("cognito-idp")
+    response = cognito.create_user_pool(
+        PoolName=user_pool_name,
+        Policies=policies,
+        UsernameAttributes=username_attributes
+    )
+
+   # Convert datetime objects to strings
+    response = json.loads(json.dumps(response, default=str))
+    
+    return {user_pool_id: response["UserPool"]["Id"], user_pool_arn: "XYZ"}
