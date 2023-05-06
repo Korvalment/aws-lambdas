1bdbd0226dd620501ab24b006e124c1ab2a6e44b
Anton <67004422+Korvalment@users.noreply.github.com>
2023-05-06 14:21:55 +0200

    Update CreateIdentityProvider.py

--- CreateIdentityProvider.py
+++ CreateIdentityProvider.py
@@ -2,15 +2,23 @@ import boto3
 
 def lambda_handler(event, context):
     client = boto3.client('cognito-idp')
+    try:
+        with open('metadata.xml') as f:
+            metadata_file = f.read()
+            provider_details = {'MetadataFile': metadata_file}
+    except FileNotFoundError:
+        try:
+            metadata_url = 'https://example.com/metadata'
+            provider_details = {'MetadataURL': metadata_url}
+        except:
+            raise Exception('metadata does not exist')
     response = client.create_identity_provider(
-        UserPoolId='eu-central-1_qIrMsVE3U',
+        UserPoolId='eu-central-1_qIrMsVE3U', # user pool id, change this
         ProviderName='TestProvider',
         ProviderType='SAML',
-        ProviderDetails={
-            'MetadataFile': 'metadata.xml'
-        },
+        ProviderDetails=provider_details,
         AttributeMapping={
-            'email': 'email',
+            'email': 'email', # email, chage this 
             'ATTRIBUTE_NAME': 'YOUR_PROVIDER_ATTRIBUTE'
         },
         IdpIdentifiers=[
